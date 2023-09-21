f1d9f98f694a94c745b3023b24151e0899856e71
Дмитрий <96023843+DmitriyGron@users.noreply.github.com>
2023-09-21 21:04:01 +0300

    Add files via upload

---
+++ homework_5_1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace homework_5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Play hamlet = new Play("Уильям Шекспир", "Цимбелин", "Трагедия", 1611);
+
+            hamlet.DisplayInfo();
+
+            hamlet = null;
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+    }
+}
---
+++ homework_5_2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace homework_5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Shop groceryShop = new Shop("СуперТЦ", "Гетманська 47", ShopType.Grocery))
+            {
+                groceryShop.DisplayInfo();
+            }
+
+        }
+    }
+}
